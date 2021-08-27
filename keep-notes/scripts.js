932011fbcdbb6b8b8507f8875361bd69ceb6e661
hugocostav <hugocostam100@outlook.com.br>
2021-08-27 19:38:32 -0300

    addBtn event / store in local storege

---
+++ keep-notes/scripts.js
@@ -0,0 +1,17 @@
+showNotes();
+
+let addBtn = document.getElementById("addBtn");
+addBtn.addEventListener('click', (e) => {
+    let addTxt = document.getElementById('addTxt');
+
+    let notes = localStorage.getItem('notes');
+    if(notes === null) {
+        notesObj = [];
+    } else {
+        notesObj = JSON.parse(notes);
+    }
+    notesObj.push(addTxt.value);
+    localStorage.setItem('notes', JSON.stringify(notesObj));
+
+    showNotes();
+})
