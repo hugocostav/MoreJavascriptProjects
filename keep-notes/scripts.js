ae38072f4694d872bf59107b98c97660f1cfc048
hugocostav <hugocostam100@outlook.com.br>
2021-08-27 21:04:09 -0300

    added showNotes method

--- keep-notes/scripts.js
+++ keep-notes/scripts.js
@@ -14,4 +14,31 @@ addBtn.addEventListener('click', (e) => {
     localStorage.setItem('notes', JSON.stringify(notesObj));
 
     showNotes();
-})
+});
+
+function showNotes() {
+    let notes = localStorage.getItem('notes');
+    if(notes === null) {
+        notesObj = [];
+    } else {
+        notesObj = JSON.parse(notes);
+    }
+    let html = "";
+    notesObj.forEach((element, index) => {
+        html += `<div class="noteCard my-2 mx-2 card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">Note ${index+1}</h5>
+          <p class="card-text">${element}</p>
+          <button id="${index}" onclick="deleteNotes(this.id)" class="btn btn-primary">Delete Note</button>
+        </div>
+      </div>`;
+    });
+
+    let notesEle = document.getElementById('notes');
+    if(notesObj.length != 0) {
+        notesEle.innerHTML = html;
+    } else {
+        notesEle.innerHTML = `Nothing to show! Use "Add a Note" section above to add notes`;
+    }
+}
+
