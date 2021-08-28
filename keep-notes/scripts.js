b94aad35b2775b9333848f812f7cd37aef128e65
hugocostav <hugocostam100@outlook.com.br>
2021-08-27 21:56:12 -0300

    added search functionalites | updated html

--- keep-notes/scripts.js
+++ keep-notes/scripts.js
@@ -54,3 +54,18 @@ function deleteNotes(index) {
     localStorage.setItem('notes', JSON.stringify(notesObj));
     showNotes();
 }
+
+let search = document.getElementById('searchTxt');
+
+search.addEventListener('input', () => {
+    let inputVal = search.value.toLowerCase();
+    let noteCards = document.getElementsByClassName('noteCard');
+    Array.from(noteCards).forEach(element => {
+        let cardTxt = element.getElementsByTagName('p')[0].innerText;
+        if(cardTxt.includes(inputVal)) {
+            element.style.display = 'block';
+        } else {
+            element.style.display = 'none';
+        }
+    })
+});
